2a9c8574f9b533782ea2c8d49a2504878a13615a
zylphrex <zylphrex@gmail.com>
2019-05-29 14:02:07 -0700

    fix: Only monkey patch once
    Make sure the monkey patching only occurs once because
    the plugin gets reloaded after the initial monkey patch (unclear why)

--- airflow_metrics/__init__.py
+++ airflow_metrics/__init__.py
@@ -1,6 +1,7 @@
 from airflow_metrics.airflow_metrics.patch_stats import patch_stats
+from airflow_metrics.utils.fn_utils import once
 
 
+@once
 def patch():
-    # TODO: called twice for some reason
     patch_stats()
