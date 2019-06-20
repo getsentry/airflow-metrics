5220ebe26e45a8965b882a2dc59cea06de197ec9
zylphrex <zylphrex@gmail.com>
2019-06-20 11:51:19 -0700

    Remove gcp api from dependencies as well as skipping monkey patching if dependencies aren't found

--- airflow_metrics/airflow_metrics/__init__.py
+++ airflow_metrics/airflow_metrics/__init__.py
@@ -15,21 +15,46 @@ def patch():
     patch_stats()
 
     if enabled('tasks'):
-        from airflow_metrics.airflow_metrics.patch_tasks import patch_tasks
-        patch_tasks()
+        monkey_patch_tasks()
 
     if enabled('thread'):
-        from airflow_metrics.airflow_metrics.patch_thread import patch_thread
-        patch_thread()
+        monkey_patch_thread()
 
     if enabled('bq'):
-        from airflow_metrics.airflow_metrics.patch_bq import patch_bq
-        patch_bq()
+        monkey_patch_bq()
 
     if enabled('gcs_to_bq'):
-        from airflow_metrics.airflow_metrics.patch_gcs_2_bq import patch_gcs_2_bq
-        patch_gcs_2_bq()
+        monkey_patch_gcs_2_bq()
 
     if enabled('requests'):
-        from airflow_metrics.airflow_metrics.patch_requests import patch_requests
-        patch_requests()
+        monkey_patch_requests()
+
+
+@swallow_error
+def monkey_patch_tasks():
+    from airflow_metrics.airflow_metrics.patch_tasks import patch_tasks
+    patch_tasks()
+
+
+@swallow_error
+def monkey_patch_thread():
+    from airflow_metrics.airflow_metrics.patch_thread import patch_thread
+    patch_thread()
+
+
+@swallow_error
+def monkey_patch_bq():
+    from airflow_metrics.airflow_metrics.patch_bq import patch_bq
+    patch_bq()
+
+
+@swallow_error
+def monkey_patch_gcs_2_bq():
+    from airflow_metrics.airflow_metrics.patch_gcs_2_bq import patch_gcs_2_bq
+    patch_gcs_2_bq()
+
+
+@swallow_error
+def monkey_patch_requests():
+    from airflow_metrics.airflow_metrics.patch_requests import patch_requests
+    patch_requests()
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='airflow-metrics',
-    version='0.1.1',
+    version='0.1.2',
     author='zylphrex',
     author_email='zylphrex@gmail.com',
     maintainer='zylphrex',
